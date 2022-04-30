8fc2f5adcb0b82e1694fa4f40753c0c47fb1ea25
hitro11 <rohitnazareth07@gmail.com>
2022-04-30 16:40:55 -0400

    Added asking price

---
+++ src/components/DownPayment.tsx
@@ -0,0 +1,18 @@
+import './DownPayment.css';
+
+const DownPayment = () => {
+  return (
+    <div className="down-payment">
+        <div className="">
+            Down Payment
+        </div>
+        <div className="">
+            <input type="text" />
+            <input type="text" />
+        </div>
+
+    </div>
+  )
+}
+
+export default DownPayment
