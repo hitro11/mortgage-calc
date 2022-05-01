ef22810af32e68077587e1dc265cd2a720b14422
hitro11 <rohitnazareth07@gmail.com>
2022-05-01 00:19:55 -0400

    added down payment UI

--- src/components/DownPayment.tsx
+++ src/components/DownPayment.tsx
@@ -3,12 +3,16 @@ import './DownPayment.css';
 const DownPayment = () => {
   return (
     <div className="down-payment">
-        <div className="">
-            Down Payment
+        <div className="dp">
+            <label className='label' >Down Payment</label>
         </div>
-        <div className="">
-            <input type="text" />
-            <input type="text" />
+        <div className="dp-minus">
+            <h1 className='minus'>-</h1>
+
+        </div>
+        <div className="dp-inputs">
+            <input className='dp-percent no-outline' type="text" />
+            <input className='dp-dollar no-outline' type="text" />
         </div>
 
     </div>
